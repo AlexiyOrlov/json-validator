19674989359a90cf23ee42857076066e4e950a78
Alexiy <alexiy.ov@gmail.com>
2020-11-02 05:36:22 +0200

    Directory input

--- src/dev/buildtool/validator/Validator.java
+++ src/dev/buildtool/validator/Validator.java
@@ -3,14 +3,42 @@ package dev.buildtool.validator;
 import dev.buildtool.json.Json5Parser;
 import dev.buildtool.json.Json5SyntaxError;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Validator {
-    public static void main(String[] strings)
+    public static void main(String[] args)
     {
-        Json5Parser json5Parser=new Json5Parser(strings[0]);
-        try {
-            json5Parser.parse();
-        } catch (Json5SyntaxError json5SyntaxError) {
-            json5SyntaxError.printStackTrace();
+        if(args.length>0) {
+            String directory=args[0];
+            Path pathDir= Paths.get(directory);
+            if(Files.isDirectory(pathDir)) {
+                try {
+                    Files.list(pathDir).forEach(path -> {
+                        String json=Json5Parser.readJson(pathDir, false);
+                        if(json!=null)
+                        {
+                            try {
+                                new Json5Parser(json).parse();
+                                System.out.println("File "+path.getFileName()+" validated.");
+                            } catch (Json5SyntaxError json5SyntaxError) {
+                                json5SyntaxError.printStackTrace();
+                                System.out.println("File "+path.getFileName()+" is incorrect");
+                            }
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                System.out.println("Supplied argument is not a directory");
+            }
+        }
+        else {
+            System.out.println("No input present");
         }
     }
 }
